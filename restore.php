51581c06f1805e51cc299cceb5b80a6e18eaa537
Asaf <asaf@lingnu.com>
2011-04-28 19:41:49 +0300

    restore : put file to restore under temp/backup

--- restore.php
+++ restore.php
@@ -17,11 +17,15 @@ $sharing_cart = sharing_cart_table::get_record_by_id($sc_id)
     or print_error('err_shared_id', 'block_sharing_cart', $return_to);
 
 // 自分が所有する共有アイテムかチェック
-$sharing_cart->user == $USER->id
+$sharing_cart->userid == $USER->id
     or print_error('err_capability', 'block_sharing_cart', $return_to);
 
 // ZIPファイル名取得
-$zip_name = $sharing_cart->file;
+// $zip_name = $sharing_cart->file;
+
+$fs = get_file_storage();
+$file = $fs->get_file_by_id($sharing_cart->fileid);
+$file->copy_content_to($CFG->dataroot."/temp/backup/backup.mbz");
 
 try {
 
@@ -35,7 +39,7 @@ try {
     $worker->beginPreferences();
 
     // ZIPファイル名設定
-    $worker->setZipName($zip_name);
+    //  $worker->setZipName($zip_name);
 
     // 設定完了
     $worker->endPreferences();
