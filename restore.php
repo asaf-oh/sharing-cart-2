a1b615185dca5ccbffbe3b5fef7afe4a0c609348
Asaf <asaf@wsdev.colman>
2011-05-01 13:51:36 +0300

    working draft (at least copy-restore-delete functions), needs urgent code refactoring

--- restore.php
+++ restore.php
@@ -2,6 +2,58 @@
 
 require_once '../../config.php';
 
+ require_once("XML/Parser.php");
+
+class XMLFileEdit extends XML_Parser {
+  protected $tagname;
+  protected $tagvalue;
+  protected $filename;
+  protected $current_element;
+  protected $tempfile;
+  
+  function XMLFileEdit($filename, $tagname, $tagvalue)
+  {
+    parent::XML_Parser();
+    $this->tagname = $tagname;
+    $this->tagvalue = $tagvalue;
+    $this->filename = $filename;
+  }
+
+  function startHandler($xp, $name, $attribs)
+  {
+    $name = strtolower($name);
+    $this->current_element = $name;
+    fwrite($this->tempfile, "<".$name);
+    foreach ($attribs as $attr_name=>$attr_value)
+      fwrite($this->tempfile," ".strtolower($attr_name)."=\"".$attr_value."\"");
+    fwrite($this->tempfile, ">");
+  }
+  function endHandler($xp, $name)
+  {
+    $name = strtolower($name);
+    fwrite($this->tempfile, "</".$name.">");
+  }
+
+ function cdataHandler($xp, $cdata)
+  {
+    if ($this->current_element == $this->tagname)
+      fwrite($this->tempfile, $this->tagvalue);
+    else
+      fwrite($this->tempfile, $cdata);
+  }
+
+ function execute()
+ {
+   $this->setInputFile($this->filename);
+   $this->tempfile = fopen($this->filename.".tmp", 'w');
+   fwrite($this->tempfile, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+   $this->parse();
+   fclose($this->tempfile);
+   rename($this->filename.".tmp", $this->filename);
+ }
+}
+
+
 //error_reporting(E_ALL);
 
 require_once($CFG->dirroot . '/backup/util/includes/restore_includes.php');
@@ -31,6 +83,16 @@ $packer->extract_to_pathname($file, $CFG->dataroot."/temp/backup/sharing_cart");
 
 $rc = new restore_controller("sharing_cart", $course_id, backup::INTERACTIVE_NO,
 			     backup::MODE_IMPORT, $USER->id, backup::TARGET_CURRENT_ADDING);
+
+if ($handle = opendir( $CFG->dataroot."/temp/backup/sharing_cart/activities")) {
+  while (false !== ($file = readdir($handle))) {
+    if ($file != "." && $file != "..") {
+      $xed = new XMLFileEdit($CFG->dataroot."/temp/backup/sharing_cart/activities/".$file."/module.xml"
+			     , "sectionnumber", $section_id);
+      $xed->execute();
+    }
+  }
+}
 
 $rc->execute_precheck(true);
 // $rc->get_precheck_results();
